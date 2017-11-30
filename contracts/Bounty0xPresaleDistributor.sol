65f3741ae7a73603c100efe17adaf08a1009b592
moodysalem <a454465c8beef70811a01489042c79b7f235df5e>
2017-11-30 09:35:51 -0500

    add comments about the math in the bounty0x presale distributor contract

--- contracts/Bounty0xPresaleDistributor.sol
+++ contracts/Bounty0xPresaleDistributor.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.4.18;
 
+import 'zeppelin-solidity/contracts/math/Math.sol';
 import 'zeppelin-solidity/contracts/math/SafeMath.sol';
 import './Bounty0xToken.sol';
 import './interfaces/Bounty0xPresaleI.sol';
@@ -11,12 +12,19 @@ contract.
 contract Bounty0xPresaleDistributor {
     using SafeMath for uint;
 
-    uint256 public constant PRESALE_FIXED_RATE = 27725850154382;      // WEI per BNTY
+    // $355.00 USD/ETH / 0.0132 USD/BNTY == 26,893.9393939394 BNTY/ETH
+    // 26,893.9393939394 BNTY/ETH * 10^-18 ETH/WEI * 10^18 BNTY/BNTYWEI == 26894 BNTYWEI/WEI
+    uint256 public constant PRESALE_FIXED_RATE = 26880; // BNTYWEI / WEI
+    // if we give all 705 ether out that's 705eth * 10^18 wei/eth * 26893 bntywei/wei * 10^-18 bnty/bntywei ==
+    // 704.976614706013306171 * 26893 == 18,958,936.0992888
+    // this contract's balance is 18.95 M so we must reduce the BNTY/WEI from 26894 to 26880 so every presale investor
+    // is compensated at the exact same ratio
+    // this is a reduction of 0.04% which does not change the price paid of the presale investors in USD
 
     Bounty0xPresaleI public deployedPresaleContract;
     Bounty0xToken public bounty0xToken;
 
-    mapping(address => bool) public tokensPaid;
+    mapping(address => uint) public tokensPaid;
 
     function Bounty0xPresaleDistributor(Bounty0xToken _bounty0xToken, Bounty0xPresaleI _deployedPresaleContract) public {
         bounty0xToken = _bounty0xToken;
@@ -34,18 +42,21 @@ contract Bounty0xPresaleDistributor {
             uint weiContributed = deployedPresaleContract.balanceOf(investorAddress);
 
             // they contributed and haven't been paid
-            if (weiContributed > 0 && !tokensPaid[investorAddress]) {
+            if (weiContributed > 0 && tokensPaid[investorAddress] == 0) {
                 // the amount of BNTY to award is the wei contributed divided by the fixed rate of WEI per BNTY
-                uint numTokens = weiContributed.div(PRESALE_FIXED_RATE);
+                uint numTokens = weiContributed.mul(PRESALE_FIXED_RATE);
+
+                // we will run out of presale tokens at the very end
+                uint tokensToPay = Math.min(bounty0xToken.balanceOf(this), numTokens);
 
                 // mark them paid first
-                tokensPaid[investorAddress] = true;
+                tokensPaid[investorAddress] = tokensToPay;
 
                 // transfer tokens to presale contributor address
-                bounty0xToken.transfer(investorAddress, numTokens);
+                bounty0xToken.transfer(investorAddress, tokensToPay);
 
                 // log the event
-                OnPreSaleBuyerCompensated(investorAddress, numTokens);
+                OnPreSaleBuyerCompensated(investorAddress, tokensToPay);
             }
         }
     }
