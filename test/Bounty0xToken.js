a4a84f2492d0e19eaf7f2b58805ef223e4167154
moodysalem <a454465c8beef70811a01489042c79b7f235df5e>
2017-12-02 16:06:22 -0500

    improve tests for the bounty0x token

--- test/Bounty0xToken.js
+++ test/Bounty0xToken.js
@@ -1,21 +1,54 @@
+import { ZERO_ADDRESS } from './util';
+
 const Bounty0xToken = artifacts.require('Bounty0xToken');
 const Bounty0xCrowdsale = artifacts.require('Bounty0xCrowdsale');
+const MiniMeTokenFactory = artifacts.require('MiniMeTokenFactory');
 
 contract('Bounty0xToken', ([ tokenController ]) => {
-  let bounty0xtoken;
-  let bounty0xCrowdsale;
+  let token;
+  let crowdsale;
+  let factory;
 
   before('get the deployed bounty0x token', async () => {
-    bounty0xtoken = await Bounty0xToken.deployed();
-    bounty0xCrowdsale = await Bounty0xCrowdsale.deployed();
+    token = await Bounty0xToken.deployed();
+    crowdsale = await Bounty0xCrowdsale.deployed();
+    factory = await MiniMeTokenFactory.deployed();
   });
 
   it('should be deployed', async () => {
-    assert.strictEqual(typeof bounty0xtoken.address, 'string');
+    assert.strictEqual(typeof token.address, 'string');
   });
 
   it('controller should be Bounty0xCrowdsale', async () => {
-    const controller = await bounty0xtoken.controller();
-    assert.strictEqual(controller, bounty0xCrowdsale.address);
+    const controller = await token.controller();
+    assert.strictEqual(controller, crowdsale.address);
+  });
+
+  it('should have a token factory', async () => {
+    const minime = await token.tokenFactory();
+    assert.strictEqual(minime, factory.address);
+  });
+
+  it('should have the constant attributes', async () => {
+    //  0x0,                        // no parent token
+    //  0,                          // no snapshot block number from parent
+    //  "Bounty0x Token",           // Token name
+    //  18   ,                      // Decimals
+    //  "BNTY",                     // Symbol
+    //  true                       // Enable transfers
+
+    const parentSnapShotBlock = await token.parentSnapShotBlock();
+    const parentToken = await token.parentToken();
+    const name = await token.name();
+    const decimals = await token.decimals();
+    const symbol = await token.symbol();
+    const transfersEnabled = await token.transfersEnabled();
+
+    assert.strictEqual(parentSnapShotBlock.valueOf(), '0');
+    assert.strictEqual(parentToken, ZERO_ADDRESS);
+    assert.strictEqual(name, 'Bounty0x Token');
+    assert.strictEqual(decimals.valueOf(), '18');
+    assert.strictEqual(symbol, 'BNTY');
+    assert.strictEqual(transfersEnabled, true);
   });
 });
