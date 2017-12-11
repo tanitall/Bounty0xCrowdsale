f5fc042d836455746527dc41d61825902af0048e
Deniz Dalkilic <denizdalkilic89@gmail.com>
2017-12-11 17:19:25 +0000

    minor-changes-in-prep-for-sale

--- contracts/KnowsConstants.sol
+++ contracts/KnowsConstants.sol
@@ -11,15 +11,14 @@ contract KnowsConstants {
     uint public constant HARD_CAP_USD = 1500000;                           // in USD the maximum total collected amount
     uint public constant MAXIMUM_CONTRIBUTION_WHITELIST_PERIOD_USD = 1500; // in USD the maximum contribution amount during the whitelist period
     uint public constant MAXIMUM_CONTRIBUTION_LIMITED_PERIOD_USD = 10000;  // in USD the maximum contribution amount after the whitelist period ends
-    uint public constant MAX_GAS_PRICE = 30 * (10 ** 9);                          // Max gas price of 30 gwei
-    uint public constant MAX_GAS = 200000;                                        // Max gas that can be sent with tx
+    uint public constant MAX_GAS_PRICE = 70 * (10 ** 9);                   // Max gas price of 70 gwei
+    uint public constant MAX_GAS = 500000;                                 // Max gas that can be sent with tx
 
     // Time constants
     uint public constant SALE_START_DATE = 1513346400;                    // in unix timestamp Dec 15th @ 15:00 CET
     uint public constant WHITELIST_END_DATE = SALE_START_DATE + 24 hours; // End whitelist 24 hours after sale start date/time
     uint public constant LIMITS_END_DATE = SALE_START_DATE + 48 hours;    // End all limits 48 hours after the sale start date
     uint public constant SALE_END_DATE = SALE_START_DATE + 4 weeks;       // end sale in four weeks
-    uint public constant VESTING_START_DATE = 1513512000;
     uint public constant UNFREEZE_DATE = SALE_START_DATE + 76 weeks;      // Bounty0x Reserve locked for 18 months
 
     function KnowsConstants() public {}
--- truffle.js
+++ truffle.js
@@ -2,15 +2,35 @@ require('babel-register');
 require('babel-polyfill');
 
 const HDWalletProvider = require('truffle-hdwallet-provider');
-const mnemonic = 'state couch illness ritual step rose west magnet arrange popular circle slam';
+const mnemonic = 'TO BE FILLED PRIOR TO DEPLOYMENT';
+const mnemonicRopsten = 'state couch illness ritual step rose west magnet arrange popular circle slam';
+const mnemonicGanache = 'candy maple cake sugar pudding cream honey rich smooth crumble sweet treat';
 
 module.exports = {
   networks: {
+    mainnet: {
+      provider: function () {
+        return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/3aPdTSUEXEXeffefJPDb');
+      },
+      network_id: 1
+    },
     ropsten: {
       provider: function () {
-        return new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/3aPdTSUEXEXeffefJPDb');
+        return new HDWalletProvider(mnemonicRopsten, 'https://ropsten.infura.io/3aPdTSUEXEXeffefJPDb');
       },
-      network_id: 3
+      network_id: 2
+    },
+    ganache: {
+      provider: function () {
+        return new HDWalletProvider(mnemonicGanache, 'http://127.0.0.1:7545');
+      },
+      network_id: 5777
+    },
+    solc: {
+      optimizer: {
+        enabled: false,
+        runs: 500
+      }
     }
   }
 };
